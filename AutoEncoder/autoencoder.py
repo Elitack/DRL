27f0a601b78efb2bc7e20462305ec658b20433a7
Elitack <lzg960702@163.com>
2017-05-24 20:36:31 +0800

    autoencoder done

--- AutoEncoder/autoencoder.py
+++ AutoEncoder/autoencoder.py
@@ -7,7 +7,6 @@ class AutoEncoder(object):
         #force needed
         self.trainData = config['trainData']
         self.hiddenSize = config['hiddenSize']
-        self.dimension = config['dimension']
         self.layerNum = len(self.hiddenSize) - 1
         #optional needed
         if 'reg' in config.keys():
@@ -34,31 +33,28 @@ class AutoEncoder(object):
 
         #dynamic static variable initial
 
-        weights_dict_s = {}
+        self.weights_dict = {}
+        self.biases_dict = {}
 
-        biases_dict_s = {}
-        '''
-        for i in range(1, self.layerNum+1):
-            weights_dict['weights'+str(i+1)] = tf.Variable(self.initializer([self.hiddenSize[i-1], self.hiddenSize[i]]), dtype=tf.float32)
-            weights_dict_s['weights'+str(i+1)] = tf.transpose(weights_dict['weights'+str(i+1)])
-            biases_dict['biases'+str(i+1)] = tf.Variable(tf.zeros(self.hiddenSize[i-1]))
-            biases_dict_s['biases' + str(i+1)] = tf.Variable(tf.zeros(self.hiddenSize[i - 1]))
-        '''
 
     def learn(self):
         weights_dict = {}
         biases_dict = {}
         for i in range(self.layerNum):
-            (weights_dict['weights' + str(i + 1)], biases_dict['biases'+str(i+1)], self.trainData) = self.learnDetail(self.hiddenSize[i], self.hiddenSize[i+1], self.trainData)
+            (self.weights_dict['weights' + str(i + 1)], self.biases_dict['biases'+str(i+1)], self.trainData) = self.learnDetail(self.hiddenSize[i], self.hiddenSize[i+1], self.trainData)
+
+    def getParameter(self):
+        return self.weights_dict, self.biases_dict
+
 
     def learnDetail(self, inputSize, hiddenSize, trainData):
         #-----------------------building network-----------------------------
-        weights1 = tf.Variable(self.initializer([self.dimension, hiddenSize]), dtype=tf.float32, name='weights1')
+        weights1 = tf.Variable(self.initializer([inputSize, hiddenSize]), dtype=tf.float32, name='weights1')
         weights2 = tf.transpose(weights1, name='weights2')
         biases1 = tf.Variable(tf.zeros(hiddenSize), name='biases1')
         biases2 = tf.Variable(tf.zeros(inputSize), name='biases2')
 
-        X  = tf.placeholder(tf.float32, shape=[None, self.dimension])
+        X  = tf.placeholder(tf.float32, shape=[None, inputSize])
 
         hidden = self.activation(tf.matmul(X, weights1)+biases1)
         outputs = tf.matmul(hidden, weights2) + biases2
@@ -72,27 +68,28 @@ class AutoEncoder(object):
 
         #----------------------training network-------------------------------
         epochs = 5
-        batchSize = 10
+        batchSize = 150
 
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
             for epoch in range(epochs):
                 ranInt = random.randint(0, len(trainData)-batchSize)
                 epochData = trainData[ranInt:ranInt+batchSize]
-                [_, weights1, biases1] = sess.run([training_op, weights1, biases1], feed_dict={X:epochData})
+                [_] = sess.run([training_op], feed_dict={X:epochData})
+
+            weights1 = weights1.eval()
+            biases1 = biases1.eval()
 
 
-
-        newData = np.dot(trainData, weights1) + biases1
+        newData = np.dot(np.array(trainData), weights1) + biases1
         newData = newData.tolist()
         return weights1, biases1, newData
 
 
 if __name__ == '__main__':
     config = {}
-    config['layerNum'] = 1
-    config['dimension'] = 1
-    config['trainData'] = np.array([float(i) for i in range(1000)]).reshape(-1, config['dimension']).tolist()
-    config['hiddenSize'] = [1000, 100]
+    config['hiddenSize'] = [1, 10, 7]
+    config['trainData'] = np.array([float(i) for i in range(1000)]).reshape(-1, config['hiddenSize'][0]).tolist()
     testSample = AutoEncoder(config=config)
     testSample.learn()
+    print (testSample.getParameter())
